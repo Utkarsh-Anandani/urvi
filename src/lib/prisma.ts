62d78f3c7afa6b8b05420fa6e1dee9089652f852
Utkarsh-Anandani <utkarshanandani9@gmail.com>
2026-04-21 18:26:18 +0530

    Fixed prisma deploy error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,32 @@
 import { PrismaClient } from "@/generated/client";
-import { PrismaPg } from "@prisma/adapter-pg"; 
-const globalForPrisma = global as unknown as {
-  client: PrismaClient; 
-}; 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL, 
-}); 
-const client =
-  globalForPrisma.client ||
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { attachDatabasePool } from "@vercel/functions";
+
+// Extend globalThis
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+// Create pool (Vercel Postgres)
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+});
+
+// Attach pool (important for Vercel serverless)
+attachDatabasePool(pool);
+
+// Create Prisma client (singleton)
+const prisma =
+  globalForPrisma.prisma ??
   new PrismaClient({
-    adapter, 
-  }); 
-if (process.env.NODE_ENV !== "production") globalForPrisma.client = client; 
-export default client; 
+    //@ts-expect-error
+    adapter: new PrismaPg(pool),
+  });
+
+// Prevent multiple instances in dev
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
